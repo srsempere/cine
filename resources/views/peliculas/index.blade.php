befc668072003bb8f9272d0990491fd6e8526a3a
Samu <samueljose.garcia@iesdonana.org>
2023-12-24 15:18:26 +0100

    Se controla si existe alguna pelicula y se muestra mensaje en tal caso en lugar de iterar

--- resources/views/peliculas/index.blade.php
+++ resources/views/peliculas/index.blade.php
@@ -13,36 +13,45 @@
                 </tr>
             </thead>
             <tbody>
-                @foreach ($peliculas as $pelicula)
+                @if ($peliculas->isEmpty())
                     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                        <td class="px-6 py-4 font-medium text-gray-900 dark:text-white">
-                            {{ $pelicula->titulo }}
-                        </td>
-                        <td class="px-6 py-4 font-medium text-gray-900 dark:text-white">
-                            <a href="{{ route('peliculas.edit', ['pelicula' => $pelicula]) }}">
-                                <x-primary-button>
-                                    Editar
-                                </x-primary-button>
-                            </a>
-                        </td>
-                        <td class="px-6 py-4 font-medium text-gray-900 dark:text-white">
-                            <form method="post" action="{{ route('peliculas.destroy', ['pelicula' => $pelicula]) }}">
-                                @csrf
-                                @method('DELETE')
-                                <x-primary-button class="bg-red-700">
-                                    Borrar
-                                </x-primary-button>
-                            </form>
-                        </td>
-                        <td class="px-6 py-4 font-medium">
-                            <a href="{{ route('peliculas.show', ['pelicula' => $pelicula]) }}">
-                                <x-primary-button class=" text-orange-400 bg-blue-700">
-                                    Ver más detalles
-                                </x-primary-button>
-                            </a>
+                        <td class="px-6 py-4 font-medium text-red-600 dark:text-white" colspan="3">
+                            No existe ninguna película actualmente.
                         </td>
                     </tr>
-                @endforeach
+                @else
+                    @foreach ($peliculas as $pelicula)
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <td class="px-6 py-4 font-medium text-gray-900 dark:text-white">
+                                {{ $pelicula->titulo }}
+                            </td>
+                            <td class="px-6 py-4 font-medium text-gray-900 dark:text-white">
+                                <a href="{{ route('peliculas.edit', ['pelicula' => $pelicula]) }}">
+                                    <x-primary-button>
+                                        Editar
+                                    </x-primary-button>
+                                </a>
+                            </td>
+                            <td class="px-6 py-4 font-medium text-gray-900 dark:text-white">
+                                <form method="post"
+                                    action="{{ route('peliculas.destroy', ['pelicula' => $pelicula]) }}">
+                                    @csrf
+                                    @method('DELETE')
+                                    <x-primary-button class="bg-red-700">
+                                        Borrar
+                                    </x-primary-button>
+                                </form>
+                            </td>
+                            <td class="px-6 py-4 font-medium">
+                                <a href="{{ route('peliculas.show', ['pelicula' => $pelicula]) }}">
+                                    <x-primary-button class=" text-orange-400 bg-blue-700">
+                                        Ver más detalles
+                                    </x-primary-button>
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
+                @endif
             </tbody>
         </table>
     </div>
