2d578b22c414a6034efb7bb3ae5cfe4dd297ae0b
Samu <samueljose.garcia@iesdonana.org>
2023-12-24 15:14:14 +0100

    Se controla si existe alguna proyección y se muestra mensaje en tal caso en lugar de iterar

--- resources/views/proyecciones/index.blade.php
+++ resources/views/proyecciones/index.blade.php
@@ -16,39 +16,47 @@
                 </tr>
             </thead>
             <tbody>
-                @foreach ($proyecciones as $proyeccion)
+                @if ($proyecciones->isEmpty())
                     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                        <td class="px-6 py-4 font-medium text-gray-900 dark:text-white">
-                            {{ $proyeccion->pelicula->titulo }}
+                        <td class="px-6 py-4 font-medium text-red-600 dark:text-white" colspan="3">
+                            No existe ninguna proyección actualmente.
                         </td>
-                        <td class="px-6 py-4 font-medium text-gray-900 dark:text-white">
-                            {{ $proyeccion->sala->numero }}
-                        </td>
-                        <td class="px-6 py-4 font-medium text-gray-900 dark:text-white">
-                            <a href="{{ route('entradas.create', ['proyeccion_id' => $proyeccion->id]) }}">
-                                <x-primary-button class="bg-green-600">
-                                    Comprar entrada
-                                </x-primary-button>
-                            </a>
-                        </td>
-                        {{-- <td class="px-6 py-4 font-medium text-gray-900 dark:text-white">
-                            <form method="post" action="{{ route('peliculas.destroy', ['pelicula' => $pelicula]) }}">
-                                @csrf
-                                @method('DELETE')
-                                <x-primary-button class="bg-red-700">
-                                    Borrar
-                                </x-primary-button>
-                            </form>
-                        </td>
-                        <td class="px-6 py-4 font-medium">
-                            <a href="{{ route('peliculas.show', ['pelicula' => $pelicula]) }}">
-                                <x-primary-button class=" text-orange-400 bg-blue-700">
-                                    Ver más detalles
-                                </x-primary-button>
-                            </a>
-                        </td> --}}
                     </tr>
-                @endforeach
+                @else
+                    @foreach ($proyecciones as $proyeccion)
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <td class="px-6 py-4 font-medium text-gray-900 dark:text-white">
+                                {{ $proyeccion->pelicula->titulo }}
+                            </td>
+                            <td class="px-6 py-4 font-medium text-gray-900 dark:text-white">
+                                {{ $proyeccion->sala->numero }}
+                            </td>
+                            <td class="px-6 py-4 font-medium text-gray-900 dark:text-white">
+                                <a href="{{ route('entradas.create', ['proyeccion_id' => $proyeccion->id]) }}">
+                                    <x-primary-button class="bg-green-600">
+                                        Comprar entrada
+                                    </x-primary-button>
+                                </a>
+                            </td>
+                            {{-- <td class="px-6 py-4 font-medium text-gray-900 dark:text-white">
+                        <form method="post" action="{{ route('peliculas.destroy', ['pelicula' => $pelicula]) }}">
+                            @csrf
+                            @method('DELETE')
+                            <x-primary-button class="bg-red-700">
+                                Borrar
+                            </x-primary-button>
+                        </form>
+                    </td>
+                    <td class="px-6 py-4 font-medium">
+                        <a href="{{ route('peliculas.show', ['pelicula' => $pelicula]) }}">
+                            <x-primary-button class=" text-orange-400 bg-blue-700">
+                                Ver más detalles
+                            </x-primary-button>
+                        </a>
+                    </td> --}}
+                        </tr>
+                    @endforeach
+                @endif
             </tbody>
         </table>
     </div>
